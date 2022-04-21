e5aaeefad7b4c951ed3a81da5616c7ceb70a3492
shongs27 <shongs27@naver.com>
2022-04-21 19:00:30 +0900

    [Feat] Resume 완성

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 - 이력서 수정
 - markdown aside 즉 callout, 이미지 처리
 
-- [ ] 블로그 글 리얼로 채우고 이쁘게 꾸미기
+- [ ] 블로그 글 리얼로 채우고 운영
 
 [PR14] 테스트 확인
 
@@ -17,12 +17,12 @@
 
 - 타이틀바에 모티게임 적용하기
 - 폼에 오류메시지 띄우기
-- https://jeonghwan-kim.github.io/dev/2022/03/29/react-form-and-formik.html#useform-%ED%9B%85%EC%9C%BC%EB%A1%9C-%EB%B6%84%EB%A6%AC
-  옆에 링크
+  https://jeonghwan-kim.github.io/dev/2022/03/29/react-form-and-formik.html#useform-%ED%9B%85%EC%9C%BC%EB%A1%9C-%EB%B6%84%EB%A6%AC
+- 조작시 실패 메시지 컴포넌트 만들기
 - react-dropzone
-- 애플과 같은 디자인 홈페이지 참조
 
 # 참고
 
+- 애플과 같은 디자인 홈페이지 참조
 - 제로초 트위터 강의 보기
 - html/css 유튜브 외국영상꺼 여러개 참조
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -12,8 +12,8 @@ const Head = styled.h1({
   margin: '2em 1em',
 });
 
-const Category = styled.div({
-  minHeight: '24em',
+const Category = styled.div(({ className }) => ({
+  minHeight: className === 'mottos' ? '18em' : '24em',
 
   borderBottom: '1px solid #e6e6e6',
   marginBottom: '2em',
@@ -22,7 +22,7 @@ const Category = styled.div({
     padding: '.5em',
     backgroundColor: '#3498db',
   },
-});
+}));
 
 const Content = styled.div({
   display: 'flex',
@@ -53,17 +53,6 @@ const bounce = keyframes`
   
   `;
 
-const fadeIn = keyframes`
-    from {
-      visibility: visible;
-      opacity: 0;
-    }
-    to {
-      visibility: visible;
-      opacity: 1;
-    }
-`;
-
 const Addon = styled.div({
   display: 'flex',
   justifyContent: 'end',
@@ -76,6 +65,17 @@ const ListMenu = styled.button(({ clicked }) => ({
   animation: clicked || `${bounce} 1.8s ease infinite alternate`,
 }));
 
+const fadeIn = keyframes`
+    from {
+      visibility: visible;
+      opacity: 0;
+    }
+    to {
+      visibility: visible;
+      opacity: 1;
+    }
+`;
+
 const ItemMenu = styled.div(({ clicked }) => ({
   position: 'relative',
   right: '1.2em',
@@ -84,6 +84,22 @@ const ItemMenu = styled.div(({ clicked }) => ({
   visibility: 'hidden',
   animation: clicked && `${fadeIn} 2s ease forwards`,
 }));
+
+const MottosList = styled.ul({
+  '& li': {
+    marginTop: '10px',
+    marginBottom: '10px',
+  },
+});
+
+const CertificationContainer = styled.div({
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+
+  '& div': {
+    margin: '10px',
+  },
+});
 
 export default function Resume() {
   const [openMenu, setOpenMenu] = useState(false);
@@ -95,6 +111,7 @@ export default function Resume() {
   return (
     <>
       <Head>😁발전하는 개발자 홍원배입니다</Head>
+
       <Category className="aboutme">
         <h2>🙋‍♂️ About me</h2>
         <Content>
@@ -138,6 +155,7 @@ export default function Resume() {
           </ContentIntroduce>
         </Content>
       </Category>
+
       <Category className="skills">
         <h2>💻 Skills</h2>
         <div style={{ paddingTop: '10px', paddingLeft: '20px' }}>
@@ -159,12 +177,28 @@ export default function Resume() {
           </ul>
         </div>
       </Category>
+
       <Category className="projects">
         <h2>👨🏼‍💻 Projects</h2>
+
+        <Container>
+          <h3>개인 블로그</h3>
+          <p>2022.01 ~</p>
+          <ul>
+            <li>TDD 개발방법론을 적용한 기술블로그</li>
+            <li>notion을 통해 정리된 글을 포스트로 렌더링</li>
+            <li>React, redux, mongoose, AWS 적용</li>
+          </ul>
+          <p>
+            지속적인 블로그 관리를 위해 TDD를 적용합니다 리팩토링을 통해 더 나은
+            UI와 학습한 글을 정리해서 쉽게 서술함으로서 다시 한번 학습하고
+            오픈소스에 기여함을 목적으로 합니다
+          </p>
+        </Container>
       </Category>
+
       <Category className="education">
         <h2>👨‍🏫 Education</h2>
-
         <Container>
           <h3>코드숨 리액트 6기</h3>
           <p>2021.12</p>
@@ -186,6 +220,13 @@ export default function Resume() {
               </a>
             </li>
           </ul>
+          <p>
+            리팩토링을 통한 코드 개선과 서로 소통할 수 있는 코드의 중요성을 배운
+            소중한 경험이었습니다. 리액트의 기본적인 원리를 바탕으로 작은
+            프로젝트들을 거듭하며 TDD 개발 방법을 익혔습니다. 무엇보다 나만이
+            이해하는 코드가 아닌 쉽고, 편하고, 명확하게 이해할 수 있는 코드에
+            대해서 고민하는 계기였습니다.
+          </p>
         </Container>
 
         <Container>
@@ -208,17 +249,51 @@ export default function Resume() {
             <li>1개월 집체교육 + 2개월 프로젝트</li>
             <b>미세먼지 농도에 따른 기상 예측 프로젝트 2등</b>
           </ul>
+          <p>
+            협업과 도메인지식의 중요성에 대해 깨닫고 프로그래밍 프로젝트를
+            전체적으로 관망할 수 있는 강력한 첫 경험이었습니다. 프로젝트 초반,
+            팀원과의 부족했던 소통으로 인해 머신러닝의 학습모델에 적용이 되지
+            않는 정제 과정이 필요 없는 변수를 가공하며 많은 시간을 소모하게
+            되었는데요, 그 후 프로젝트를 리빌딩하면서 서로의 업무에 대한 관심과
+            대화를 통한 협동의 중요성을 확인하고 개선시켜 빠르게 시간을 절약해
+            나갔고 협동을 통해 학습모델을 위한 적절한 변수를 선정하여 예측율을
+            70퍼센트까지 끌어올릴 수 있었습니다. 협업과 서로를 위한 피드백이
+            중요하다는 것을 깨닫는 경험이었습니다.
+          </p>
         </Container>
       </Category>
 
-      {/* //💬 */}
+      <Category className="certification">
+        <h2>📌 자격증</h2>
+        <CertificationContainer>
+          <Container>
+            <h3>토익스피킹 LV6</h3>
+            <p>주관: ETS / 취득일 : 2020.09</p>
+          </Container>
+          <Container>
+            <h3>한국사 1급</h3>
+            <p>주관: 국사편찬위원회 / 취득일 : 2019.06</p>
+          </Container>
+          <Container>
+            <h3>DSAC 3등급</h3>
+            <p>데이터사이언티스트 능력검정</p>
+            <p>주관: 한국생산성본부 / 취득일 : 2019.05</p>
+          </Container>
+          <Container>
+            <h3>정보처리기능사</h3>
+            &nbsp;
+            <p>주관: 한국산업인력공단 </p>
+          </Container>
+        </CertificationContainer>
+      </Category>
 
       <Category className="mottos">
-        <h2>📌 Mottos</h2>
-        <ul>
-          <li>책만 읽기에는 시간이 너무 없다</li>
+        <h2>💬 Mottos</h2>
+        <MottosList>
+          <li>매일 하는 학습</li>
           <li>'척' 하지말자</li>
-        </ul>
+          <li>메타인지를 통해 분명하고 명확하게 학습하자</li>
+        </MottosList>
       </Category>
 
       <Addon>
